ccd32ba3c202d7d2a1f9be1c86b1a758b67043a8
DESKTOP-EFB2T8L\sunlang <khy20030930@gmail.com>
2024-07-24 18:26:29 +0900

    HTTP 응답 데이터 - API JSON

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ResponseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloDate = new HelloData();
+        helloDate.setUsername("김효영");
+        helloDate.setAge(22);
+
+        //{"username":"김효영", "age":22}
+
+        String result = objectMapper.writeValueAsString(helloDate);
+
+        response.getWriter().write(result);
+    }
+}
